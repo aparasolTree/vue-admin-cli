8bfc578b1ff3beb03517ef8a1c64f397ff9ab184
aparasolTree <LikeParasolTree@163.com>
2023-05-25 10:25:01 +0800

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,40 @@
-import { ConfigEnv, loadEnv } from 'vite'
-import alias from './vite/alias'
-import { parseEnv } from './vite/util'
-import setupPlugins from './vite/plugins'
-import { visualizer } from 'rollup-plugin-visualizer'
+import { ConfigEnv, loadEnv, defineConfig } from "vite";
+import alias from "./vite/alias";
+import { parseEnv } from "./vite/util";
+import setupPlugins from "./vite/plugins";
+import { visualizer } from "rollup-plugin-visualizer";
 
-export default ({ command, mode }: ConfigEnv) => {
-  const isBuild = command == 'build'
-  const root = process.cwd()
-  const env = parseEnv(loadEnv(mode, root))
+export default defineConfig(({ command, mode }: ConfigEnv) => {
+    const isBuild = command == "build";
+    const root = process.cwd();
+    const env = parseEnv(loadEnv(mode, root));
 
-  return {
-    plugins: [...setupPlugins(isBuild, env), visualizer()],
-    resolve: {
-      alias,
-    },
-    build: {
-      rollupOptions: {
-        emptyOutDir: true,
-        output: {
-          manualChunks(id: string) {
-            if (id.includes('node_modules')) {
-              return id.toString().split('node_modules/')[1].split('/')[0].toString()
-            }
-          },
+    return {
+        base: "/vue-admin-cli/",
+        plugins: [...setupPlugins(isBuild), visualizer()],
+        resolve: {
+            alias,
         },
-      },
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          rewrite: (path: string) => path.replace(/^\/api/, ''),
+        build: {
+            rollupOptions: {
+                emptyOutDir: true,
+                output: {
+                    manualChunks(id: string) {
+                        if (id.includes("node_modules")) {
+                            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+                        }
+                    },
+                },
+            },
         },
-      },
-    },
-  }
-}
+        server: {
+            proxy: {
+                "/api": {
+                    target: env.VITE_API_URL,
+                    changeOrigin: true,
+                    rewrite: (path: string) => path.replace(/^\/api/, ""),
+                },
+            },
+        },
+    };
+});
